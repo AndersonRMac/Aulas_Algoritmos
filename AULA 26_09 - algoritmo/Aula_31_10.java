48e12796fdba542edd969f4f61e22ae26c125c87
AndersonRMac <anderson.ramosmaciel@gmail.com>
2022-10-31 20:16:27 -0300

    Aula sorteio (terminar)

---
+++ AULA 26_09 - algoritmo/Aula_31_10.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Aula_31_10 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int saldo;
+        String resposta;
+
+        System.out.println("Você quer depositar quanto de saldo?");
+        saldo = entrada.nextInt();
+
+        System.out.println("Deseja Sortear? s/n");
+        resposta = entrada.next().toLowerCase();
+
+        String[] animais = {"cachorro", "boi", "gato","coelho", "vaca", "galinha"};
+        String[] correlacao = {"seguranca", "redbull", "miado","pascoa", "leite", "ovo"};
+
+        if (resposta.equals("s")){
+
+
+        }
+
+
+
+
+    }
+}
+
